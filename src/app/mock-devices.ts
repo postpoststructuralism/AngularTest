9f2d3b425a9239e9d76e3576233cb3f7b06a61a1
Kevin Stinson <postpoststruct@outlook.com>
2021-09-26 20:58:55 -0400

    Cleanup mock data.

--- src/app/mock-devices.ts
+++ src/app/mock-devices.ts
@@ -1,14 +1,14 @@
-import { Device } from './device';
+import { Device } from "./device";
 
 export const DEVICES: Device[] = [
-  { id: 1, name: 'Device 1', status:'Available', temperature:39 },
-  { id: 2, name: 'Device 2', status:'Offline', temperature:9 },
-  { id: 3, name: 'Device 3', status:'Available', temperature:3 },
-  { id: 4, name: 'Device 4', status:'Offline', temperature:11 },
-  { id: 5, name: 'Device 5', status:'Available', temperature:15 },
-  { id: 6, name: 'Device 6', status:'Available', temperature:22 },
-  { id: 7, name: 'Device 7', status:'Offline', temperature:6 },
-  { id: 8, name: 'Device 8', status:'Available', temperature:17 },
-  { id: 9, name: 'Device 9', status:'Offline', temperature:24 },
-  { id: 10, name: 'Device 10', status:'Available', temperature:12 }
+  { "id": 1, "name": "Device 1", "status":"Available", "temperature":39 },
+  { "id": 2, "name": "Device 2", "status":"Offline", "temperature":9 },
+  { "id": 3, "name": "Device 3", "status":"Available", "temperature":3 },
+  { "id": 4, "name": "Device 4", "status":"Offline", "temperature":11 },
+  { "id": 5, "name": "Device 5", "status":"Available", "temperature":15 },
+  { "id": 6, "name": "Device 6", "status":"Available", "temperature":22 },
+  { "id": 7, "name": "Device 7", "status":"Offline", "temperature":6 },
+  { "id": 8, "name": "Device 8", "status":"Available", "temperature":17 },
+  { "id": 9, "name": "Device 9", "status":"Offline", "temperature":24 },
+  { "id": 10, "name": "Device 10", "status":"Available", "temperature":12 }
 ];
