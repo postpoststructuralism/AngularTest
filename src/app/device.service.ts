04a4f994fd96780b4d3f2efaa75a6b261be9cfea
Kevin Stinson <postpoststruct@outlook.com>
2021-09-28 20:18:03 -0400

    Add some error handling to service.

--- src/app/device.service.ts
+++ src/app/device.service.ts
@@ -3,6 +3,7 @@ import { Device } from './device';
 import { Observable, BehaviorSubject, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -21,17 +22,33 @@ export class DeviceService {
     private messageService: MessageService) { }
 
   getDevices(): Observable<Device[]> {
-    var devices = this.http.get<Device[]>(this.baseUrl);
+    var devices = this.http.get<Device[]>(this.baseUrl)
+      .pipe(catchError(this.handleError<Device[]>('getDevices', [])));
     return devices;
   }
 
   getDevice(id: number): Observable<Device> {
     const url = `${this.baseUrl}/${id}`;
-    return this.http.get<Device>(url);
+    return this.http.get<Device>(url)
+      .pipe(catchError(this.handleError<Device>(`getDevice id${id}`)));
   }
 
   changeMessage(message: string) {
     this.messageSource.next(message);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   private log(message: string) {
